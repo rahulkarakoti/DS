ba2fbdb69c053834f1e6aa044a0351081563254c
rahulsingh <117975372+rahulkarakoti@users.noreply.github.com>
2023-05-12 21:30:59 +0530

    modified:   linked list/insertion.c

--- linked list/insertion.c
+++ linked list/insertion.c
@@ -18,18 +18,18 @@ void linkedlist_view(struct node *ptr) // dispaly the linked list
     }
 }
 
-struct node *insertionatfirst(struct node *head, int data) // case 1: insert element at first index
+struct node *insertion_atfirst(struct node *head, int data) // case 1: insert element at first index
 {
     struct node *ptr = (struct node *)malloc(sizeof(struct node));
     ptr->next = head;
     ptr->data = data;
 }
 
-struct node *insertionatindex(struct node *head, int index, int data) // case 2: insert element at any index
+struct node *insertion_atindex(struct node *head, int index, int data) // case 2: insert element at any index
 {
     struct node *ptr = (struct node *)malloc(sizeof(struct node));
-    int i = 0;
     struct node *p = head;
+    int i = 0;
 
     while (i != index - 1)
     {
@@ -39,6 +39,30 @@ struct node *insertionatindex(struct node *head, int index, int data) // case 2:
     ptr->data = data;
     ptr->next = p->next;
     p->next = ptr;
+    return head;
+}
+
+struct node *insertion_atend(struct node *head,int data){  //case:3 insert at end
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data=data;
+    struct node *p = head;
+
+    while(p->next!=NULL)
+    {
+        p = p->next;
+    }
+    p->next=ptr;
+    ptr->next=NULL;
+    return head;
+}
+
+struct node *insertion_afternode(struct node *head,struct node *prevnode,int data){//case 4: insert after the node 
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data=data;
+    
+    ptr->next=prevnode->next;
+    prevnode->next=ptr;
+
     return head;
 }
 
@@ -74,24 +98,33 @@ int main()
         printf("\nenter elemnt:");
         scanf("%d", &x);
 
-        printf("1.insert at first\n2.insert at index\nenter choice:");
+        printf("1.insert at first\n2.insert at index\n3.insert at end\n4.insert at second index\n5.exit\nenter choice:");
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
 
-            head = insertionatfirst(head, x); // 999 is inserted at 1st index
+            head = insertion_atfirst(head, x); // x is inserted at 1st index
             printf("\n");
             linkedlist_view(head); // display the list
             break;
         case 2:
             printf("enter index:");
             scanf("%d", &index);
-            head = insertionatindex(head, index, x); // 921 is inserted at 3rd index
+            head = insertion_atindex(head, index, x); // x is inserted at  index
             linkedlist_view(head);
             break;
-    
+        case 3:
+            head = insertion_atend(head,x); // x is inserted at 3rd index
+            printf("\n");
+            linkedlist_view(head);
+            break;
+    case 4:
+            head = insertion_afternode(head,third,x); // x is inserted at 2nd index
+            printf("\n");
+            linkedlist_view(head);
+            break;
         }
-    } while (ch<3);
+    } while (ch<5);
     return 0;
 }
